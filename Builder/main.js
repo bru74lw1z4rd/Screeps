37be21530480773b8eaf92f8ad6ea83efd8cf96d
BrutalWizard <66485293+bru74lw1z4rd@users.noreply.github.com>
2020-12-29 16:03:37 +0300

    Imrpoved syntax

--- Builder/main.js
+++ Builder/main.js
@@ -2,13 +2,12 @@ var roleHarvester = require('role.harvester');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-
-    for(var name in Game.creeps) {
+    for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
+        if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'builder') {
+        if (creep.memory.role == 'builder') {
             roleBuilder.run(creep);
         }
     }
