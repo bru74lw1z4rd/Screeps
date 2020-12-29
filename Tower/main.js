46a2b89eade6137a662250d14ae1fc186e2c816c
BrutalWizard <66485293+bru74lw1z4rd@users.noreply.github.com>
2020-12-29 18:07:53 +0300

    Create main.js

---
+++ Tower/main.js
@@ -0,0 +1,26 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+    var tower = Game.getObjectById('3cb85295f17269ab5ab23f43');
+    if( tower) {
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
